df945474e2333eed8540d7369ef5e9fb04112297
deyvisonborges <deyvisonsilvaborges@gmail.com>
2024-11-02 18:40:33 -0300

    [tests]: utilitario pra renderizar ui com thema e redux

---
+++ src/utils/test/renderWithTheme.tsx
@@ -0,0 +1,19 @@
+import { store } from '@/store'
+import { ThemeProvider } from '@/styles/theme/theme.provider'
+import { render, RenderOptions } from '@testing-library/react'
+import { ReactElement } from 'react'
+import { Provider as ReduxProvider } from 'react-redux'
+
+type CustomRenderProps = Omit<RenderOptions, 'queries'>
+
+export const renderWithTheme = (
+  ui: ReactElement,
+  { ...renderOptions }: CustomRenderProps
+) => {
+  return render(
+    <ReduxProvider store={store}>
+      <ThemeProvider>{ui}</ThemeProvider>
+    </ReduxProvider>,
+    renderOptions
+  )
+}
