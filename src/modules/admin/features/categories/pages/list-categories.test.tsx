80423efc37215785b9defb2c0b8f0593b0dcc55c
deyvisonborges <deyvisonsilvaborges@gmail.com>
2024-11-22 17:17:42 -0300

    feat: categories - add test from filter categories

--- src/modules/admin/features/categories/pages/list-categories.test.tsx
+++ src/modules/admin/features/categories/pages/list-categories.test.tsx
@@ -1,6 +1,6 @@
 import { ListCategoriesPage } from './list-categories.page'
 import { renderWithProviders } from '@/utils/test/renderWithProviders'
-import { screen, waitFor } from '@testing-library/react'
+import { fireEvent, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 // import { handlersFail, mockServer } from '../mocks/handlers'
 
@@ -46,7 +46,39 @@ describe('ListCategoriesPage', () => {
     expect(secondPageItem).toBeInTheDocument()
   })
 
-  it('', () => {})
+  it('should handle filter change', async () => {
+    /**
+     * A api mock retorna 2 items por paginas
+     * Logo, ele so vai encontrar o Item 1 e o Item 2
+     * Ao aplicar o busca por nome para Item 1, ele deve retornar somente ele no DOM
+     */
+    const { getByText, getByPlaceholderText } = renderWithProviders(
+      <ListCategoriesPage />
+    )
+
+    // Certifica-se de que o Item 1 e o Item 2 estão presentes inicialmente
+    // const firstPageItem = await screen.findByText(/Item 1/i)
+    // const secondPageItem = await screen.findAllByText(/Item 2/i)
+    // expect(firstPageItem).toBeInTheDocument()
+    // expect(secondPageItem.length).toBeGreaterThan(0)
+
+    // logica simplicada
+    await waitFor(() => {
+      expect(getByText(/Item 1/)).toBeInTheDocument()
+      expect(getByText(/Item 2/)).toBeInTheDocument()
+    })
+
+    // Realiza a busca pelo nome "Item 1"
+    const searchInput = getByPlaceholderText(/Buscar por nome/i)
+    fireEvent.change(searchInput, { target: { value: 'Item 1' } })
+
+    // Garante que as alterações esperadas no DOM sejam verificadas de forma assíncrona.
+    await waitFor(() => {
+      expect(screen.getByText(/Item 1/i)).toBeInTheDocument()
+      // queryByText retorna null se o elemento não for encontrado, permitindo verificar sua ausência sem lançar um erro.
+      expect(screen.queryByText(/Item 2/i)).not.toBeInTheDocument()
+    })
+  })
 
   // // https://www.ivstudio.com/blog/mock-service-worker
   // it('should render error state', async () => {
